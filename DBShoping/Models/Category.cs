1251fd059b5f81dfcf91013f4ba642ad37884b8b
Zayniddinov Zuhriddin <94095165+zuhriddinzayniddinov@users.noreply.github.com>
2022-12-20 18:27:29 +0500

    CODE RUB: Fix Category Model

--- DBShoping/Models/Category.cs
+++ DBShoping/Models/Category.cs
@@ -5,4 +5,5 @@ class Category
   public Guid CategoryId { get; set; }
   public string CategoryName { get; set; }
   public string Description { get; set; }
+  public ICollection<Product> Products { get; set; }
 }
