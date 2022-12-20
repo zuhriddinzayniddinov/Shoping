fd11b1a32ef1777f8de16cc3751667fbf11b7dfd
Diyor <shumtakadiyor@gmail.com>
2022-12-20 15:55:18 +0500

    DATA: Create Customer

--- DBShoping/Models/Customer.cs
+++ DBShoping/Models/Customer.cs
@@ -8,5 +8,12 @@ namespace DBShoping.Models
 {
     internal class Customer
     {
+        public Guid CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
     }
 }
