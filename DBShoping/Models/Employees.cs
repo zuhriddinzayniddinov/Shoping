bc47e652f03579df5e0f00e7ffd872ee3c2a1df4
Zayniddinov Zuhriddin <94095165+zuhriddinzayniddinov@users.noreply.github.com>
2022-12-20 15:20:59 +0500

    DATA: Create Employee

---
+++ DBShoping/Models/Employees.cs
@@ -0,0 +1,13 @@
+namespace DBShoping;
+class Employee
+{
+  public Guid EmployeeId { get; set; }
+  public string  LastName { get; set; }
+  public string FirstName { get; set; }
+  public DateTimeOffset BirthDate { get; set; }
+  public DateTimeOffset HireDate { get; set; }
+  public string Region { get; set; }
+  public string City { get; set; }
+  public string Country { get; set; }
+  public string HomePhone { get; set; }
+}
